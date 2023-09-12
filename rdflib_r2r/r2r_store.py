6968610853414a095148286b1544097cc9dcdf27
vickyli117 <63615626+vickyli117@users.noreply.github.com>
2023-09-12 16:19:39 +0800

    Update r2r_store.py small changes on col_n3 int

--- rdflib_r2r/r2r_store.py
+++ rdflib_r2r/r2r_store.py
@@ -546,7 +546,12 @@ class R2RStore(Store):
             n3col = '"' + value + ('"^^' + dt)
             n3col.original = dbcol
             return n3col
-        
+         if isinstance(dbcol.type, sqltypes.INT):
+            dt = XSD.integer.n3() # if this can run
+            value = sqlfunc.cast(dbcol, sqltypes.VARCHAR)
+            n3col = '"' + value + ('"^^' + dt)
+            n3col.original = dbcol
+            return n3col
         ## TODO: create n3 literal for integers!
 
         return dbcol
